987d8b50be4d2305e5a60a2129bc6eb0337a553f
root <root@GLaDOS.(none)>
2014-03-16 04:27:05 -0400

    Updating changes from nginx setup

--- sites/default/temp_files/update-extraction-8435c1cd/authcache/modules/authcache_esi/authcache_esi_debug.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  Drupal.behaviors.authcacheEsiDebug = {
-    attach: function (context, settings) {
-      if (settings.authcacheEsiDebug) {
-        $('#authcache-esi-debug-table').load(settings.authcacheEsiDebug);
-      }
-    }
-  };
-}(jQuery));
